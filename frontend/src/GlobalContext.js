f0fcd324fff12e03c0469aa6317cc4934e6ecdff
Florian Nelles <florian.nelles@cronn.de>
2025-12-21 14:07:46 +0100

    Weihnachtsquiz disabled, small improvements 3

--- frontend/src/GlobalContext.js
+++ frontend/src/GlobalContext.js
@@ -1,5 +1,6 @@
 // export const baseUrl = 'http://localhost:8080/';
-export const baseUrl = 'http://164.92.162.134:8080/';
+// export const baseUrl = 'http://164.92.162.134:8080/';
+export const baseUrl = '/';
 
 
 export const dataDragonVersion = '14.24.1';
